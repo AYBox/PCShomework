f454286f2f5366aa30191654d1e46efc5c9e3799
AvrahamYBock <yonabock@gmail.com>
2017-10-25 22:18:40 -0400

    quiz initial commit

---
+++ JavaScript/HW_78/map.js
@@ -0,0 +1,15 @@
+"use strict";
+var anArray = [2, 4, 6];
+function doubleIt(num){
+    return num * 2;
+}
+
+function myMap(theArray, callback){
+    var newArray = [];
+    theArray.forEach(function(element){
+        newArray.push(callback(element));
+    });
+    return newArray;
+}
+
+console.log("myMap(anArray, doubleIt): ", myMap(anArray, doubleIt));
