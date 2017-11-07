a623a1e146a2013cea3b1b2d87e3598634c770dd
AvrahamYBock <yonabock@gmail.com>
2017-11-07 12:18:28 -0500

    HW_81 updated

--- JavaScript/HW_81/stopWatch.js
+++ JavaScript/HW_81/stopWatch.js
@@ -1,9 +1,5 @@
-(function () {
+function createStopwatch() {
     "use strict";
-
-    var timeSpan = document.getElementById("timeSpan");
-    var startStopButton = document.getElementById("startStopButton");
-    var resetButton = document.getElementById("resetButton");
 
     var startString = "Start";
     var stopString = "Stop";
@@ -11,6 +7,31 @@
     var intervalId;
     var startTime;
     var savedTimeElapsed = 0;
+
+    var mainDiv = document.createElement("div");
+    var displayDiv = document.createElement("div");
+    var timeSpan = document.createElement("span");
+    var buttonDiv = document.createElement("div");
+    var startStopButton = document.createElement("button");
+    var resetButton = document.createElement("button");
+
+    document.body.appendChild(mainDiv);
+    mainDiv.appendChild(displayDiv);
+    mainDiv.appendChild(buttonDiv);
+    displayDiv.appendChild(timeSpan);
+    buttonDiv.appendChild(startStopButton);
+    buttonDiv.appendChild(resetButton);
+
+    mainDiv.setAttribute("style", "position: absolute; right: 50%; top: 50%; display: inline-block;background-color: lightblue; border: solid darkblue 2px; padding: 10px; min-width: 180px; min-height: 75px; text-align: center;");
+    document.body.style = "box-sizing: border-box";
+    displayDiv.style.marginBottom = '10px';
+    timeSpan.style = 'font-size: 31px; font-family: Miriam Fixed, Times New Roman;';
+    startStopButton.style = 'width: 45%';
+    resetButton.style = 'width: 45%';
+
+    timeSpan.innerHTML = "00:00:00.00";
+    startStopButton.innerHTML = "Start";
+    resetButton.innerHTML = "Reset";
 
     function timeFormat(ms) {
         return new Date(ms).toISOString().slice(11, -2);
@@ -43,4 +64,4 @@
         savedTimeElapsed = 0;
         timeSpan.innerHTML = "00:00:00.00";
     });
-}());
+}
